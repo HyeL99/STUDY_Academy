529d5cc6560c77b3169db792f0d3ce4c5e14cbc1
HyeL99 <kanghr8149@gmail.com>
2022-10-26 13:21:29 +0900

    d.221026

---
+++ _07.REACT/04.simpleToDo/src/components/TodoBoard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import TodoItem from './TodoItem';
+
+const TodoBoard = ({todoList}) => {
+    return (
+        <>
+            <h1>Todo List</h1>
+            {todoList.map(item => <TodoItem item={item} />)}
+        </>
+    )
+}
+
+export default TodoBoard;
