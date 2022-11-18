0d6bea82a7bc9258de7387c96dfa5f12ded46024
Hyeri Kang <kanghr8149@gmail.com>
2022-11-18 13:16:43 +0900

    d.221118-am

---
+++ _07.REACT/19.✔weatherApp/03.UI setting/components/WeatherBox.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const WeatherBox = () => {
+  return (
+    <div className='menuBox'>
+      <div className='text-light h1'>서울</div>
+      <h2 className='text-warning h1'>30도 / 120화씨</h2>
+      <h3 className='text-info h2'>맑은하늘</h3>
+    </div>
+  )
+}
+
+export default WeatherBox
