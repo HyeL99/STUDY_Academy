056a364eff29ad4c598ff649df9524ad375406c3
HyeL99 <101614314+HyeL99@users.noreply.github.com>
2022-11-23 22:33:01 +0900

    d.221123

---
+++ _11.PWA/scheduleApp_ 순서/첫날/js/time.js
@@ -0,0 +1,14 @@
+$(function(){
+  $('#contentsBtn .timeTable').on('click',function(){
+    $('#timeTablePage').css('top','50px');
+  });
+  $('#timerBtn').on('click',function(){
+    $('#timeTablePage').css('top','50px');
+  });
+  $('#todayTimeTableArticle').on('click',function(){
+    $('#timeTablePage').css('top','50px');
+  });
+  $('#timeTablePage .closeBtn').on('click',function(){
+    $('#timeTablePage').css('top','100%');
+  });
+})
