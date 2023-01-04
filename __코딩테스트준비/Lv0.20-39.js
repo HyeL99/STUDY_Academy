210d12c491add222b048fcad2b67a9d7fb675b2d
Hyeri Kang <kanghr8149@gmail.com>
2023-01-04 17:42:12 +0900

    d.230104

--- __코딩테스트준비/Lv0.20-39.js
+++ __코딩테스트준비/Lv0.20-39.js
@@ -219,9 +219,80 @@ function solution35(letter) {
     '-.--':'y','--..':'z'
   }
 
-  letter.split(' ');
-  
-  return ant5 + ant3 + ant1;
+  let array = letter.split(' ');
+  array = array.map(item => morse[item])
+  return array.join('');
 }
 
-console.log('35',solution35(999));
+console.log('35',solution35(".... . .-.. .-.. ---"));
+
+/*
+  36. 모스부호 (1)
+  가위는 2 바위는 0 보는 5로 표현합니다. 가위 바위 보를 내는 순서대로 나타낸 문자열 rsp가 매개변수로 주어질 때, rsp에 저장된 가위 바위 보를 모두 이기는 경우를 순서대로 나타낸 문자열을 return하도록 solution 함수를 완성해보세요.
+*/
+
+function solution36(rsp) {
+  let array = rsp.split('');
+  array = array.map(item => getRsp(item))
+  return array.join('');
+}
+const getRsp = (n) => {
+  switch (n){
+    case '2': return '0';
+    case '5': return '2';
+    case '0': return '5';
+  }
+}
+
+console.log('36',solution36("205"));
+
+/*
+  37. 구슬을 나누는 경우의 수
+  머쓱이는 구슬을 친구들에게 나누어주려고 합니다. 구슬은 모두 다르게 생겼습니다. 머쓱이가 갖고 있는 구슬의 개수 balls와 친구들에게 나누어 줄 구슬 개수 share이 매개변수로 주어질 때, balls개의 구슬 중 share개의 구슬을 고르는 가능한 모든 경우의 수를 return 하는 solution 함수를 완성해주세요.
+*/
+
+function solution37(balls, share) {
+  return Math.round(factorial(balls) / (factorial(balls - share) * factorial(share)));
+}
+const factorial = (num) => {
+  let rst = 1;
+  for(let i = 2; i <= num; i++){
+    rst *= i;
+  }
+  return rst;
+}
+
+console.log('37',solution37(5,3));
+
+/*
+  38. 점의 위치 구하기
+  사분면은 한 평면을 x축과 y축을 기준으로 나눈 네 부분입니다. 사분면은 아래와 같이 1부터 4까지 번호를매깁니다.
+      x 좌표와 y 좌표가 모두 양수이면 제1사분면에 속합니다.
+      x 좌표가 음수, y 좌표가 양수이면 제2사분면에 속합니다.
+      x 좌표와 y 좌표가 모두 음수이면 제3사분면에 속합니다.
+      x 좌표가 양수, y 좌표가 음수이면 제4사분면에 속합니다.
+  x 좌표 (x, y)를 차례대로 담은 정수 배열 dot이 매개변수로 주어집니다. 좌표 dot이 사분면 중 어디에 속하는지 1, 2, 3, 4 중 하나를 return 하도록 solution 함수를 완성해주세요.
+*/
+
+function solution38(dot) {
+  return dot[0] > 0 ? (dot[1] > 0 ? 1 : 4) : (dot[1] > 0 ? 2 : 3);
+}
+
+console.log('38',solution38([-7, 9]));
+
+/*
+  39. 2차원으로 만들기
+  정수 배열 num_list와 정수 n이 매개변수로 주어집니다. num_list를 다음 설명과 같이 2차원 배열로 바꿔 return하도록 solution 함수를 완성해주세요.
+
+  num_list가 [1, 2, 3, 4, 5, 6, 7, 8] 로 길이가 8이고 n이 2이므로 num_list를 2 * 4 배열로 다음과 같이 변경합니다. 2차원으로 바꿀 때에는 num_list의 원소들을 앞에서부터 n개씩 나눠 2차원 배열로 변경합니다.
+*/
+
+function solution39(num_list, n) {
+  let answer = [];
+  for(let i=0; i<num_list.length; i+=n){
+    answer.push(num_list.slice(i, i+n));
+  }
+  return answer;
+}
+
+console.log('39',solution39([100, 95, 2, 4, 5, 6, 18, 33, 948],3));
